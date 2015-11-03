b831bf3b6736c87b3194c27b3be79241ece8b812
0higgsboson <0higgsboson@users.noreply.github.com>
2015-11-02 23:42:56 -0800

    Update install_run.sh

--- Scripts/Setup/install_run.sh
+++ Scripts/Setup/install_run.sh
@@ -2,10 +2,10 @@
 
 # Assumptions
 # 1. Default host name is tenzing-red.  If different, update hadoop_cluster_installer to reflect this before running it.
+# 2. Copy ssh public key into github
 
 sudo -i
 ./hadoop_cluster_installer.sh
 source /etc/environment
 ./sherpa_installer.sh
-
 

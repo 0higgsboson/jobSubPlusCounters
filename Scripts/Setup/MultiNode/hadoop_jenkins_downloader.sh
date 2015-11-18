e09def8bd31afc5d2f59251179aa73501cc4b188
Ismail Dalgic <ismail@theperformancesherpa.com>
2015-11-18 01:46:26 +0000

    Added script for downloading hadoop onto the jenkins work area

---
+++ Scripts/Setup/MultiNode/hadoop_jenkins_downloader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Assumptions
+# 1. Uses jenkins account
+
+installation_base_dir=~jenkins/jobs/Sherpa/workspace
+hadoop_dir=$installation_base_dir/hadoop
+HADOOP_VERSION=2.6.0
+
+mkdir $hadoop_dir
+cd $hadoop_dir
+rm hadoop-${HADOOP_VERSION}.tar.gz
+wget https://archive.apache.org/dist/hadoop/core/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz
+tar -xzf hadoop-${HADOOP_VERSION}.tar.gz

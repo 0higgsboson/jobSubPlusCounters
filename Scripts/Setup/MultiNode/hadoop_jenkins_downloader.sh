c54385ea34c494476e6977a964fe581604f71d61
Ismail Dalgic <ismail@theperformancesherpa.com>
2015-11-18 01:58:41 +0000

    Needed the source tar file, not the binaries for this script.

--- Scripts/Setup/MultiNode/hadoop_jenkins_downloader.sh
+++ Scripts/Setup/MultiNode/hadoop_jenkins_downloader.sh
@@ -5,11 +5,11 @@
 #
 
 installation_base_dir=~jenkins/jobs/Sherpa/workspace
-hadoop_dir=$installation_base_dir/hadoop
+hadoop_dir=$installation_base_dir/hadoop-src
 HADOOP_VERSION=2.6.0
 
 mkdir $hadoop_dir
 cd $hadoop_dir
-rm hadoop-${HADOOP_VERSION}.tar.gz
-wget https://archive.apache.org/dist/hadoop/core/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz
-tar -xzf hadoop-${HADOOP_VERSION}.tar.gz
+rm hadoop-${HADOOP_VERSION}-src.tar.gz
+wget https://archive.apache.org/dist/hadoop/core/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}-src.tar.gz
+tar -xzf hadoop-${HADOOP_VERSION}-src.tar.gz

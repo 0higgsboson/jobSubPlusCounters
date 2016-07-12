1e83b9aba8d249a65e79aca151093e78bc83a0a5
Akhtar <akhtar@theperformancesherpa.com>
2016-07-12 08:17:13 -0700

    Updated Dev Scripts due to build changes

--- Scripts/Dev/hive_compile_deploy.sh
+++ Scripts/Dev/hive_compile_deploy.sh
@@ -37,23 +37,9 @@ cd $hive_client_src_dir/hiveClientSherpa
 # Copies custom jars into Hive's lib dir
 print "Copying jars into Hive's lib dir ..."
 
-echo "rm ${hive_home}/lib/hive-cli-1.2.1.jar"
-rm ${hive_home}/lib/hive-cli-1.2.1.jar
+rm ${hive_home}/lib/hive-cli*.jar
+rm ${hive_home}/lib/hive-exec*.jar
 
-echo "rm ${hive_home}/lib/hive-exec-1.2.1.jar"
-rm ${hive_home}/lib/hive-exec-1.2.1.jar
-
-
-echo "cp cli/target/hive-cli-1.2.1.jar ${hive_home}/lib/hive-cli-1.2.1.jar"
-cp cli/target/hive-cli-1.2.1.jar ${hive_home}/lib/hive-cli-1.2.1.jar
-
-echo "cp ql/target/hive-exec-1.2.1.jar ${hive_home}/lib/hive-exec-1.2.1.jar"
-cp ql/target/hive-exec-1.2.1.jar ${hive_home}/lib/hive-exec-1.2.1.jar
-
-echo "cp ${common_src_dir}/TzCtCommon/target/TzCtCommon-1.0-jar-with-dependencies.jar /root/cluster/hive/apache-hive-1.2.1-bin/lib/"
-cp ${common_src_dir}/TzCtCommon/target/TzCtCommon-1.0-jar-with-dependencies.jar /root/cluster/hive/apache-hive-1.2.1-bin/lib/
-
-
-cp cli/target/hive-cli-1.2.1.jar /opt/sherpa/lib/
-cp ql/target/hive-exec-1.2.1.jar /opt/sherpa/lib/
-cp ${common_src_dir}/TzCtCommon/target/TzCtCommon-1.0-jar-with-dependencies.jar /opt/sherpa/lib/
+cp cli/target/hive-cli*.jar /opt/sherpa/lib/hive-cli.jar
+cp ql/target/hive-exec*.jar /opt/sherpa/lib/hive-exec.jar
+cp ${common_src_dir}/TzCtCommon/target/TzCtCommon*jar-with-dependencies.jar /opt/sherpa/lib/TzCtCommon-jar-with-dependencies.jar
--- Scripts/Dev/mr_compile_deploy.sh
+++ Scripts/Dev/mr_compile_deploy.sh
@@ -30,5 +30,5 @@ printHeader "Deploying MR Client"
 cd ${mr_client_src_dir}/
 
 print "Copying Jars ..."
-cp ${mr_client_src_dir}/target/hadoop-mapreduce-client-core-${HADOOP_VERSION}.jar /opt/sherpa/lib/
-cp ${common_src_dir}/TzCtCommon/target/TzCtCommon-1.0-jar-with-dependencies.jar /opt/sherpa/lib/
+cp ${mr_client_src_dir}/target/hadoop-mapreduce-client-core*.jar /opt/sherpa/lib/hadoop-mapreduce-client-core.jar
+cp ${common_src_dir}/TzCtCommon/target/TzCtCommon*jar-with-dependencies.jar /opt/sherpa/lib/TzCtCommon-jar-with-dependencies.jar

723dad68b1de85aaec5714e403a75e3951861074
Ismail Dalgic <ismail@theperformancesherpa.com>
2017-08-24 01:20:53 +0000

    Modified cron script for saving 3 ROI files

--- Scripts/Dev/tenzing-analysis/azure-sql-reports.sh
+++ Scripts/Dev/tenzing-analysis/azure-sql-reports.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-/root/tenzing-analysis/roi-to-csv-win.py > ~sherpa/roi-nh.csv
+/root/tenzing-analysis/roi-memory-to-csv-win.py > ~sherpa/roi-mem-nh.csv
+/root/tenzing-analysis/roi-cpu-to-csv-win.py > ~sherpa/roi-cpu-nh.csv
+/root/tenzing-analysis/roi-latency-to-csv-win.py > ~sherpa/roi-latency-nh.csv
 /root/tenzing-analysis/reports-selected-to-csv-win.py > ~sherpa/reports.tsv

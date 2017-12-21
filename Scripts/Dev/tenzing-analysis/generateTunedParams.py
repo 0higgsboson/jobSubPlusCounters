6620c52a4ec2b564b5b9ddb5bb716cab7bdc3af5
root <root@azure-cicd2-03.jb5z054wwjfuvjgmxezzufoyph.dx.internal.cloudapp.net>
2017-12-21 19:42:13 +0000

    generateTunedParams with arguments

--- Scripts/Dev/tenzing-analysis/generateTunedParams.py
+++ Scripts/Dev/tenzing-analysis/generateTunedParams.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
 
+import sys
 import json
 import collections
 from collections import OrderedDict
 
 configs = OrderedDict()
 
-minval=0
-maxval=100
-defaultval=50
+
+n = int(float(sys.argv[1]))
+minval= int(float(sys.argv[2]))
+maxval= int(float(sys.argv[3]))
+defaultval= int(float(sys.argv[4]))
+
 valtype='DOUBLE'
 stepSize=1
 namePrefix='p'
 
-n = 10
 
 for i in range(0,n):
     name = namePrefix + "{0:0>3}".format(i+1)

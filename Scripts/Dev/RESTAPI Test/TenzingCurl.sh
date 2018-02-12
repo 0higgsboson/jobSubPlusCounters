1982960f01b3b3c4d04a5dcbd2faa3e0898f2049
svish2602 <svish2602@gmail.com>
2018-02-12 10:14:51 -0800

    Curl script file added for REST API run
    Added sh file to run the curl script for Tenzing API

--- Scripts/Dev/RESTAPI Test/TenzingCurl.sh
+++ Scripts/Dev/RESTAPI Test/TenzingCurl.sh
@@ -6,13 +6,23 @@ source configurations.sh
 
 echo $source
 
-curl -i http://$HOST_NAME:$PORT/tenzing-services/api/1.0/version
+echo -e "\n --- tenzig version REST call ---\n"
+
+curl -i http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/version
+
+echo -e "\n --- tzctcommonversion REST call ---\n"
 
 curl -i  http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/tzctcommonversion
 
+echo -e "\n --- jobresult REST call ---\n"
+
 curl -i  -H "Content-Type: application/json" -X POST -d '{"clusterID":"sherpa"}' http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/jobresult
 
+echo -e "\n --- psmanaged REST call --- \n"
+
 curl -i  -H "Content-Type: application/json" -X POST -d '{"userName":"sherpa1","queueName":"aaaa"}' http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/psmanaged
+
+echo -e "\n --- sync REST call --- \n"
 
 curl -i -H "Content-Type:application/json" -X POST -d '{"clusterID":"sherpa","tenzingsSequenceNo":"99999","cluseteinfo":{"id":"0","clusterID":"sherpa","hadoopVersion":"2.7.1"} }' http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/sync
 

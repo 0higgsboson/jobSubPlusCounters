eeec841e6ba3ea2f9f9a216a3ea915b635989da1
0higgsboson <sid@theperformancesherpa.com>
2018-04-12 23:35:29 +0000

    Separated ports and hosts for CA and Tz

--- Scripts/Dev/RESTAPI Test/TenzingCurl.sh
+++ Scripts/Dev/RESTAPI Test/TenzingCurl.sh
@@ -9,74 +9,74 @@ echo $source
 
 echo -e "\n --- tenzig version REST call ---\n"
 sleep 3
-echo -e "# REST URL: http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/version \n"
+echo -e "# REST URL: http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/version \n"
 
-curl -i http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/version
+curl -i http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/version
 
 echo -e "\n\n  --- tzctcommonversion REST call ---\n"
 sleep 3
-echo -e "# REST URL: http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/tzctcommonversion \n"
+echo -e "# REST URL: http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/tzctcommonversion \n"
 
-curl -i  http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/tzctcommonversion
+curl -i  http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/tzctcommonversion
 
 echo -e "\n\n --- jobresult REST call ---\n"
 sleep 3
-echo -e "# REST URL: http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/jobresult \n"
+echo -e "# REST URL: http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/jobresult \n"
 
-curl -i  -H "Content-Type: application/json" -X POST -d '{"clusterID":"sherpa"}' http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/jobresult
+curl -i  -H "Content-Type: application/json" -X POST -d '{"clusterID":"sherpa"}' http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/jobresult
 
 echo -e "\n\n --- psmanaged REST call --- \n"
 sleep 3
-echo -e "# REST URL: http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/psmanaged \n"
+echo -e "# REST URL: http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/psmanaged \n"
 
-curl -i  -H "Content-Type: application/json" -X POST -d '{"userName":"sherpa1","queueName":"aaaa"}' http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/psmanaged
+curl -i  -H "Content-Type: application/json" -X POST -d '{"userName":"sherpa1","queueName":"aaaa"}' http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/psmanaged
 
 
 echo -e "\n\n --- sync REST call --- \n"
 sleep 3
-echo -e "# REST URL:  http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/sync \n" 
+echo -e "# REST URL:  http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/sync \n" 
 
-curl -i -H "Content-Type:application/json" -X POST -d '{"clusterID":"sherpa","tenzingsSequenceNo":"99999","cluseteinfo":{"id":"0","clusterID":"sherpa","hadoopVersion":"2.7.1"} }' http://${HOST_NAME}:${PORT}/tenzing-services/api/1.0/sync
+curl -i -H "Content-Type:application/json" -X POST -d '{"clusterID":"sherpa","tenzingsSequenceNo":"99999","cluseteinfo":{"id":"0","clusterID":"sherpa","hadoopVersion":"2.7.1"} }' http://${TENZING_HOST_NAME}:${TENZING_PORT}/tenzing-services/api/1.0/sync
 
 
 echo -e "\n\n --- heartbeat REST call --- \n"
 sleep 3
-echo -e "# RESt URL:  http://${HOST_NAME}:${PORT}/ca-services/api/1.0/heartbeat \n"
+echo -e "# RESt URL:  http://${TENZING_HOST_NAME}:${TENZING_PORT}/ca-services/api/1.0/heartbeat \n"
 
-curl -i  -H "Content-Type: application/json" -X POST -d '{"configsServed":"2","metadataReceived":"5","heartbeatInterval":"10","clusterID":"sherpa","timestamp":"1342049220104L"}' http://${HOST_NAME}:${PORT}/ca-services/api/1.0/heartbeat
+curl -i  -H "Content-Type: application/json" -X POST -d '{"configsServed":"2","metadataReceived":"5","heartbeatInterval":"10","clusterID":"sherpa","timestamp":"1342049220104L"}' http://${TENZING_HOST_NAME}:${TENZING_PORT}/ca-services/api/1.0/heartbeat
 
 
 echo -e "\n\n ------ CA Service REST API Call Starts  -------\n\n"
 
 echo -e " --- CA service REST call --- \n"
 sleep 3
-echo -e "# REST URL: http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/version \n"
+echo -e "# REST URL: http://${CA_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/version \n"
 
-curl -i http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/version
+curl -i http://${TENZING_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/version
 
 echo -e "\n\n --- tzctcommonversion REST Call --- \n"
 sleep 3
-echo -e "# RESt URL: http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/tzctcommonversion \n"
+echo -e "# RESt URL: http://${CA_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/tzctcommonversion \n"
 
-curl -i http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/tzctcommonversion
+curl -i http://${CA_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/tzctcommonversion
 
 echo -e "\n\n --- tunedparams REST call --- \n"
 sleep 3
-echo -e "# REST URL: http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/tunedparams \n"
+echo -e "# REST URL: http://${CA_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/tunedparams \n"
 
-curl -i -H "Content-Type:application/json" -X POST -d '{"workloadId":"sherpa","clientType":"CA"}' http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/tunedparams
+curl -i -H "Content-Type:application/json" -X POST -d '{"workloadId":"sherpa","clientType":"CA"}' http://${TENZING_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/tunedparams
 
 echo -e "\n\n --- jobresult REST call --- \n"
 sleep 3
-echo -e "# REST URL: http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/jobresult  \n"
+echo -e "# REST URL: http://${CA_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/jobresult  \n"
 
-curl -i  -H "Content-Type: application/json" -X POST -d '{"clusterID":"sherpa"}' http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/jobresult
+curl -i  -H "Content-Type: application/json" -X POST -d '{"clusterID":"sherpa"}' http://${CA_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/jobresult
 
 echo -e "\n\n --- psmanaged REST call --- \n"
 sleep 3
-echo -e "# REST URL: http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/psmanaged  \n"
+echo -e "# REST URL: http://${CA_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/psmanaged  \n"
 
-curl -i  -H "Content-Type: application/json" -X POST -d '{"userName":"sherpa1","queueName":"aaaa"}' http://${HOST_NAME}:${CA_PORT}/ca-services/api/1.0/psmanaged
+curl -i  -H "Content-Type: application/json" -X POST -d '{"userName":"sherpa1","queueName":"aaaa"}' http://${CA_HOST_NAME}:${CA_PORT}/ca-services/api/1.0/psmanaged
 
 echo -e "\n\n"
 
--- Scripts/Dev/RESTAPI Test/configurations.sh
+++ Scripts/Dev/RESTAPI Test/configurations.sh
@@ -2,6 +2,7 @@
 
 #setting Port and Host for the RestAPI test
 
-HOST_NAME=52.161.10.108
-PORT=8580
+TENZING_HOST_NAME=localhost #52.161.10.108
+CA_HOST_NAME=localhost #52.161.10.108
+TENZING_PORT=8580
 CA_PORT=8680
